7ecb31344836866c0cef0fb890917afe066c3f9f
Micky Lephend <idpwf1@gmail.com>
2024-12-16 12:42:45 -0500

    build script fix

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 mkdir -p info
 
 echo BUILD INFO > info/info.txt
-echo JENKINS JOB $"JOB_NAME" >> info/info.txt
-echo LAST GIT COMMIT HASH $"GIT_COMMIT" >> info/info.txt
-echo FREE PARAM $"MY_STRING" >> info/info.txt
+echo JENKINS JOB "$JOB_NAME" >> info/info.txt
+echo LAST GIT COMMIT HASH "$GIT_COMMIT" >> info/info.txt
+echo FREE PARAM "$MY_STRING" >> info/info.txt
 
