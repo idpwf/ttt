89966425a732170e0252255fe07104a8f20339b1
Micky Lephend <idpwf1@gmail.com>
2024-12-24 00:24:45 -0500

    logging

--- demo.py
+++ demo.py
@@ -4,11 +4,14 @@ import unittest
 
 import os
 
+import logging
+
 
 if __name__ == "__main__":
   unittest.main()
 
 class DemoTest(unittest.TestCase):
+  logger = logging.getLogger(__name__)
 
   def test_success1(self):
     self.assertTrue(True)
@@ -17,5 +20,5 @@ class DemoTest(unittest.TestCase):
     self.assertFalse(False)
 
   def test_is_env_var_set1(self):
-    print("this should be printed somewhere")
+    self.logger.warning("I'm _so_ logging this")
     self.assertTrue(os.getenv("ENV_VAR_1") == "True")
